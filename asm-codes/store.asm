9492ebee43563cc6882b08f9b6218bc115878203
Zeeshan Chowdhary <im_zshan@Zeeshans-MacBook-Air.local>
2023-02-09 15:29:15 +0530

    store.asm modified

--- asm-codes/store.asm
+++ asm-codes/store.asm
@@ -1,10 +1,10 @@
-/* Store 8-bit data in memory using direct addressing. */
+: Store 8-bit data in memory using direct addressing.
 
     MVI A, 49H      : "Store 49H in the accumulator"  
     STA 2501H       : "Copy accumulator contents at address 2501H"  
     HLT             : "Stop"
 
-/* Store 8-bit data in memory using indirect addressing. */
+: Store 8-bit data in memory using indirect addressing.
 
     LXI H       : "Load H-L pair with 2501H"  
     MVI M       : "Store 49H in memory location pointed by H-L register pair (2501H)"  
